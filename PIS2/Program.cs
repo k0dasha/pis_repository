e8892bb951208b8a45a0bd933550be0870495b40
Даша <kolegovadasa2@gmail.com>
2025-10-06 00:23:39 +0700

    Разбиение основных двух методов. Добавление виртуального метода для парсинга строки вне конструкторов классов

--- PIS2/Program.cs
+++ PIS2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography.X509Certificates;
@@ -14,7 +15,7 @@ namespace PIS2
         public string Name { get; set; }
         public string Theme { get; set; }
         public DateTime Date { get; set; }
-        public Student(string input)
+        public virtual void ParseString(string input)
         {
             var parts = input.Split('"');
 
@@ -22,13 +23,20 @@ namespace PIS2
             Theme = parts[3];
             Date = DateTime.Parse(parts[parts.Length - 1].Replace("дата", ""));
         }
+        public Student(string input)
+        {
+            ParseString(input);
+        }
     }
     public class Bachelor : Student
     {
         public int EducationPeriod { get; set; }
-        public Bachelor(string input) : base(input)
+        public override void ParseString(string input)
         {
             var parts = input.Split('"');
+            Name = parts[1];
+            Theme = parts[3];
+            Date = DateTime.Parse(parts[parts.Length - 1].Replace("дата", ""));
 
             if (parts.Length >= 6)
             {
@@ -36,17 +44,29 @@ namespace PIS2
                 EducationPeriod = int.Parse(periodStr);
             }
         }
+        public Bachelor(string input) : base(input)
+        {
+            ParseString(input);
+        }
     }
     public class Master : Student
     {
         public string InternshipPlace { get; set; }
-        public Master(string input) : base(input)
+        public override void ParseString(string input)
         {
             var parts = input.Split('"');
+            Name = parts[1];
+            Theme = parts[3];
+            Date = DateTime.Parse(parts[parts.Length - 1].Replace("дата", ""));
+
             if (parts.Length >= 6)
             {
                 InternshipPlace = parts[5];
             }
+        }
+        public Master(string input) : base(input)
+        {
+            ParseString(input);
         }
     }
     class Program
@@ -56,20 +76,21 @@ namespace PIS2
             var students = LoadStudents("input.txt");
             PrintInfo(students);
         }
-        static void PrintInfo(List<Student> students1)
+        static void PrintInfo(List<Student> studentsList)
         {
-            foreach (var student in students1)
+            foreach (var student in studentsList)
             {
-                string info = ($"{student.Name}, {student.Theme}, {student.Date.ToString("yyyy.MM.dd")}");
-                if (student is Bachelor b)
-                {
-                    Console.WriteLine(info + $" {b.EducationPeriod}");
-                }
-                else if (student is Master m)
-                {
-                    Console.WriteLine(info + $" {m.InternshipPlace}");
-                }
+                PrintStudent(student);
             }
+        }
+        static void PrintStudent(Student student)
+        {
+            string info = $"{student.Name}, {student.Theme}, {student.Date:yyyy.MM.dd}";
+
+            if (student is Bachelor b)
+                Console.WriteLine($"{info}, {b.EducationPeriod}");
+            else if (student is Master m)
+                Console.WriteLine($"{info}, {m.InternshipPlace}");
         }
         static List<Student> LoadStudents(string filePath)
         {
@@ -77,21 +98,31 @@ namespace PIS2
 
             foreach (var line in File.ReadLines(filePath))
             {
+                var student = ParseStudent(line);
+                if (student != null)
+                    result.Add(student);
+            }
+            return result;
+        }
+        static Student ParseStudent(string line)
+        {
+            foreach (var i in line)
+            {
                 var parts = line.Split(new char[] { ' ' });
 
                 if (parts.Length > 3)
                 {
                     if ((parts[5] as string) == null)
                     {
-                        result.Add(new Bachelor(line));
+                        return (new Bachelor(line));
                     }
                     else
                     {
-                        result.Add(new Master(line));
+                        return (new Master(line));
                     }
                 }
             }
-            return result;
+            return null;
         }
     }
 }
